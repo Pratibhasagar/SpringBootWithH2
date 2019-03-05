75bc8d421e86d42325e55bd525dca0e1a854ac29
Pratibhasagar V <pratibav@thoughtworks.com>
2019-03-05 09:50:42 +0530

    Pratibha | Add ResourceNotFoundException. Update getAllUsers() in UserController to throw exception.

--- src/main/java/com/dbs/h2/demo/controller/UserController.java
+++ src/main/java/com/dbs/h2/demo/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.dbs.h2.demo.controller;
 
+import com.dbs.h2.demo.exception.ResourceNotFoundException;
 import com.dbs.h2.demo.model.User;
 import com.dbs.h2.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,7 +22,11 @@ public class UserController {
     }
 
     @GetMapping(value = "/users")
-    List<User> getAllUsers() {
-        return userService.getAllUsers();
+    List<User> getAllUsers() throws ResourceNotFoundException {
+        List<User> allUsers = userService.getAllUsers();
+        if (allUsers.isEmpty()) {
+            throw new ResourceNotFoundException("No Users Found");
+        }
+        return allUsers;
     }
 }
